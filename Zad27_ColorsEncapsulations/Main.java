2c2c06a73125e575439604cefe73e10bc2deb030
Dominik Skórka <dominik.skorka88@gmail.com>
2023-03-05 15:19:57 +0100

    Encapsulation Colors for text

---
+++ Zad27_ColorsEncapsulations/Main.java
@@ -0,0 +1,89 @@
+public class Main {
+
+    private final String ANSI_BLACK = "\u001B[30m";
+    private final String ANSI_RED = "\u001B[31m";
+    private final String ANSI_GREEN = "\u001B[32m";
+    private final String ANSI_YELLOW = "\u001B[33m";
+    private final String ANSI_BLU = "\u001B[34m";
+    private final String ANSI_PURPLE = "\u001B[35m";
+    private final String ANSI_CYAN = "\u001B[36m";
+    private final String ANSI_WHITE = "\u001B[37m";
+    private final String ANSI_RESET = "\u001B[0m";
+    //---------------------------------------------------------------------------
+    private final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+    private final String ANSI_RED_BACKGROUND = "\u001B[41m";
+    private final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+    private final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+    private final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+    private final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+    private final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+    private final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
+    //---------------------------------------------------------------------------
+    public String getANSI_BLU() {
+        return ANSI_BLU;
+    }
+
+    public String getANSI_GREEN() {
+        return ANSI_GREEN;
+    }
+
+    public String getANSI_RED() {
+        return ANSI_RED;
+    }
+
+    public String getANSI_RESET() {
+        return ANSI_RESET;
+    }
+    public String getANSI_YELLOW() {
+        return ANSI_YELLOW;
+    }
+
+    public String getANSI_BLACK() {
+        return ANSI_BLACK;
+    }
+
+    public String getANSI_PURPLE() {
+        return ANSI_PURPLE;
+    }
+
+    public String getANSI_CYAN() {
+        return ANSI_CYAN;
+    }
+
+    public String getANSI_WHITE() {
+        return ANSI_WHITE;
+    }
+
+    public String getANSI_RED_BACKGROUND() {
+        return ANSI_RED_BACKGROUND;
+    }
+
+    public String getANSI_WHITE_BACKGROUND() {
+        return ANSI_WHITE_BACKGROUND;
+    }
+
+    public String getANSI_BLACK_BACKGROUND() {
+        return ANSI_BLACK_BACKGROUND;
+    }
+
+    public String getANSI_GREEN_BACKGROUND() {
+        return ANSI_GREEN_BACKGROUND;
+    }
+
+    public String getANSI_YELLOW_BACKGROUND() {
+        return ANSI_YELLOW_BACKGROUND;
+    }
+
+    public String getANSI_BLUE_BACKGROUND() {
+        return ANSI_BLUE_BACKGROUND;
+    }
+
+    public String getANSI_PURPLE_BACKGROUND() {
+        return ANSI_PURPLE_BACKGROUND;
+    }
+
+    public String getANSI_CYAN_BACKGROUND() {
+        return ANSI_CYAN_BACKGROUND;
+    }
+}
