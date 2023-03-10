0bd5a428c3666ae22cb4e8599e98fb3a66c6f2e8
Dominik Skórka <dominik.skorka88@gmail.com>
2023-03-10 19:09:41 +0100

    Class Math

---
+++ Zad28_ClassMath/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = -5;
+        int b = 11;
+        System.out.println(Math.max(a,b));
+        System.out.println(Math.min(a,b));
+        System.out.println(Math.abs(a));
+        System.out.println(Math.pow(2,3));
+        System.out.println(Math.sqrt(16));
+
+        System.out.println(Math.round(5.5));
+        System.out.println(Math.ceil(5.0001));
+        System.out.println(Math.floor(5.99999));
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+
+    }
+}
