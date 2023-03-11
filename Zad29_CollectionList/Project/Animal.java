7b492653f316cf405e7a7a0c111cb449ee2bfca9
Dominik Skórka <dominik.skorka88@gmail.com>
2023-03-11 15:01:31 +0100

    Collection List

---
+++ Zad29_CollectionList/Project/Animal.java
@@ -0,0 +1,8 @@
+package Project;
+
+public class Animal {
+    Animal(String name){
+        this.name = name;
+    }
+    public String name = "Garfierld";
+}
---
+++ Zad29_CollectionList/Project/Main.java
@@ -0,0 +1,65 @@
+package Project;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Main {
+    public static void main(String[] args) {
+       Animal cat1 = new Animal("Stella");
+       Animal cat2 = new Animal("Lucy");
+       Animal cat3 = new Animal("Lily");
+
+        Animal dog1 = new Animal("Winston");
+        Animal dog2 = new Animal("Riley");
+        Animal dog3 = new Animal("Chip");
+
+        ArrayList list = new ArrayList();
+
+        list.add(cat1);
+        list.add(cat2);
+        list.add(cat3);
+
+        ArrayList<Animal> list2 = new ArrayList<Animal>(); //safer
+        list2.add(dog1);
+        list2.add(dog2);
+        list2.add(dog3);
+
+        System.out.println("------------------");
+        for(Object k : list2){
+            System.out.println(((Animal)k).name);
+        }
+        System.out.println("------------------");
+//-----------------------------------------------------------
+        System.out.println("------------------");
+        for(Object k : list){
+            System.out.println(((Animal)k).name);
+        }
+        System.out.println("------------------");
+
+        System.out.println(list.get(0));
+        System.out.println(((Animal)list.get(0)).name);
+
+        System.out.println(list.size());
+        System.out.println(list.contains(cat2));
+
+//--------------------------------------------------------
+                list.remove(cat2);
+                list.remove(0);
+        System.out.println("------------------");
+        for(Object k : list){
+            System.out.println(((Animal)k).name);
+        }
+        System.out.println("------------------");
+        System.out.println(list.size());
+
+ //-------------------------------------------------------
+        list.clear();
+        System.out.println("------------------");
+        for(Object k : list){
+            System.out.println(((Animal)k).name);
+        }
+        System.out.println("------------------");
+        System.out.println(list.size());
+    }
+}
